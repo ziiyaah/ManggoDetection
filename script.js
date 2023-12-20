b288d394ce35c6dae8a4c882f90b3d62998a39ed
Ziyah Sakinah <151896484+ziiyaah@users.noreply.github.com>
2023-12-20 20:32:04 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,421 @@
+$(function() {
+	retrieveDefaultValuesFromLocalStorage();
+	setupButtonListeners();
+});
+
+var infer = function() {
+	$('#output').html("Inferring...");
+	$("#resultContainer").show();
+	$('html').scrollTop(100000);
+
+	getSettingsFromForm(function(settings) {
+		settings.error = function(xhr) {
+			$('#output').html("").append([
+				"Error loading response.",
+				"",
+				"Check your API key, model, version,",
+				"and other parameters",
+				"then try again."
+			].join("\n"));
+		};
+
+		$.ajax(settings).then(function(response) {
+			if(settings.format == "json") {
+				var pretty = $('<pre>');
+				var formatted = JSON.stringify(response, null, 4)
+
+				pretty.html(formatted);
+				$('#output').html("").append(pretty);
+				$('html').scrollTop(100000);
+			} else {
+				var arrayBufferView = new Uint8Array(response);
+				var blob = new Blob([arrayBufferView], {
+					'type': 'image\/jpeg'
+				});
+				var base64image = window.URL.createObjectURL(blob);
+
+				var img = $('<img/>');
+				img.get(0).onload = function() {
+					$('html').scrollTop(100000);
+				};
+				img.attr('src', base64image);
+				$('#output').html("").append(img);
+			}
+		});
+	});
+};
+
+var retrieveDefaultValuesFromLocalStorage = function() {
+	try {
+		var api_key = localStorage.getItem("rf.api_key");
+		var model = localStorage.getItem("rf.model");
+		var format = localStorage.getItem("rf.format");
+
+		if (api_key) $('#api_key').val(api_key);
+		if (model) $('#model').val(model);
+		if (format) $('#format').val(format);
+	} catch (e) {
+		// localStorage disabled
+	}
+
+	$('#model').change(function() {
+		localStorage.setItem('rf.model', $(this).val());
+	});
+
+	$('#api_key').change(function() {
+		localStorage.setItem('rf.api_key', $(this).val());
+	});
+
+	$('#format').change(function() {
+		localStorage.setItem('rf.format', $(this).val());
+	});
+};
+
+var setupButtonListeners = function() {
+	// run inference when the form is submitted
+	$('#inputForm').submit(function() {
+		infer();
+		return false;
+	});
+
+	// make the buttons blue when clicked
+	// and show the proper "Select file" or "Enter url" state
+	$('.bttn').click(function() {
+		$(this).parent().find('.bttn').removeClass('active');
+		$(this).addClass('active');
+
+		if($('#computerButton').hasClass('active')) {
+			$('#fileSelectionContainer').show();
+			$('#urlContainer').hide();
+		} else {
+			$('#fileSelectionContainer').hide();
+			$('#urlContainer').show();
+		}
+
+		if($('#jsonButton').hasClass('active')) {
+			$('#imageOptions').hide();
+		} else {
+			$('#imageOptions').show();
+		}
+
+		return false;
+	});
+
+	// wire styled button to hidden file input
+	$('#fileMock').click(function() {
+		$('#file').click();
+	});
+
+	// grab the filename when a file is selected
+	$("#file").change(function() {
+		var path = $(this).val().replace(/\\/g, "/");
+		var parts = path.split("/");
+		var filename = parts.pop();
+		$('#fileName').val(filename);
+	});
+};
+
+var getSettingsFromForm = function(cb) {
+	var settings = {
+		method: "POST",
+	};
+
+	var parts = [
+		"https://detect.roboflow.com/",
+		$('#model').val(),
+		"/",
+		$('#version').val(),
+		"?api_key=" + $('#api_key').val()
+	];
+
+	var classes = $('#classes').val();
+	if(classes) parts.push("&classes=" + classes);
+
+	var confidence = $('#confidence').val();
+	if(confidence) parts.push("&confidence=" + confidence);
+
+	var overlap = $('#overlap').val();
+	if(overlap) parts.push("&overlap=" + overlap);
+
+	var format = $('#format .active').attr('data-value');
+	parts.push("&format=" + format);
+	settings.format = format;
+
+	if(format == "image") {
+		var labels = $('#labels .active').attr('data-value');
+		if(labels) parts.push("&labels=on");
+
+		var stroke = $('#stroke .active').attr('data-value');
+		if(stroke) parts.push("&stroke=" + stroke);
+
+		settings.xhr = function() {
+			var override = new XMLHttpRequest();
+			override.responseType = 'arraybuffer';
+			return override;
+		}
+	}
+
+	var method = $('#method .active').attr('data-value');
+	if(method == "upload") {
+		var file = $('#file').get(0).files && $('#file').get(0).files.item(0);
+		if(!file) return alert("Please select a file.");
+
+		getBase64fromFile(file).then(function(base64image) {
+			settings.url = parts.join("");
+			settings.data = base64image;
+
+			console.log(settings);
+			cb(settings);
+		});
+	} else {
+		var url = $('#url').val();
+		if(!url) return alert("Please enter an image URL");
+
+		parts.push("&image=" + encodeURIComponent(url));
+
+		settings.url = parts.join("");
+		console.log(settings);
+		cb(settings);
+	}
+};
+
+var getBase64fromFile = function(file) {
+    return new Promise(function(resolve, reject) {
+        var reader = new FileReader();
+        reader.readAsDataURL(file);
+        reader.onload = function() {
+            resolve(reader.result);
+        };
+        reader.onerror = function(error) {
+            reject(error);
+        };
+    });
+};
+
+var resizeImage = function(base64Str) {
+	return new Promise(function(resolve, reject) {
+		var img = new Image();
+		img.src = base64Str;
+		img.onload = function(){
+			var canvas = document.createElement("canvas");
+			var MAX_WIDTH = 1500;
+			var MAX_HEIGHT = 1500;
+			var width = img.width;
+			var height = img.height;
+			if (width > height) {
+				if (width > MAX_WIDTH) {
+					height *= MAX_WIDTH / width;
+					width = MAX_WIDTH;
+				}
+			} else {
+				if (height > MAX_HEIGHT) {
+					width *= MAX_HEIGHT / height;
+					height = MAX_HEIGHT;
+				}
+			}
+			canvas.width = width;
+			canvas.height = height;
+			var ctx = canvas.getContext('2d');
+			ctx.drawImage(img, 0, 0, width, height);
+			resolve(canvas.toDataURL('image/jpeg', 1.0));  
+		};
+    
+	});	
+};
+/*jshint esversion:6*/
+
+$(function () {
+    const video = $("video")[0];
+
+    var model;
+    var cameraMode = "environment"; // or "user"
+
+    const startVideoStreamPromise = navigator.mediaDevices
+        .getUserMedia({
+            audio: false,
+            video: {
+                facingMode: cameraMode
+            }
+        })
+        .then(function (stream) {
+            return new Promise(function (resolve) {
+                video.srcObject = stream;
+                video.onloadeddata = function () {
+                    video.play();
+                    resolve();
+                };
+            });
+        });
+
+    var publishable_key = "A56kviwV4IxClAdxcSsX";
+    var toLoad = {
+        model: "manggasegar_manggatidaksegar",
+        version: 1
+    };
+
+    const loadModelPromise = new Promise(function (resolve, reject) {
+        roboflow
+            .auth({
+                publishable_key: publishable_key
+            })
+            .load(toLoad)
+            .then(function (m) {
+                model = m;
+                resolve();
+            });
+    });
+
+    Promise.all([startVideoStreamPromise, loadModelPromise]).then(function () {
+        $("body").removeClass("loading");
+        resizeCanvas();
+        detectFrame();
+    });
+
+    var canvas, ctx;
+    const font = "16px sans-serif";
+
+    function videoDimensions(video) {
+        // Ratio of the video's intrisic dimensions
+        var videoRatio = video.videoWidth / video.videoHeight;
+
+        // The width and height of the video element
+        var width = video.offsetWidth,
+            height = video.offsetHeight;
+
+        // The ratio of the element's width to its height
+        var elementRatio = width / height;
+
+        // If the video element is short and wide
+        if (elementRatio > videoRatio) {
+            width = height * videoRatio;
+        } else {
+            // It must be tall and thin, or exactly equal to the original ratio
+            height = width / videoRatio;
+        }
+
+        return {
+            width: width,
+            height: height
+        };
+    }
+
+    $(window).resize(function () {
+        resizeCanvas();
+    });
+
+    const resizeCanvas = function () {
+        $("canvas").remove();
+
+        canvas = $("<canvas/>");
+
+        ctx = canvas[0].getContext("2d");
+
+        var dimensions = videoDimensions(video);
+
+        console.log(
+            video.videoWidth,
+            video.videoHeight,
+            video.offsetWidth,
+            video.offsetHeight,
+            dimensions
+        );
+
+        canvas[0].width = video.videoWidth;
+        canvas[0].height = video.videoHeight;
+
+        canvas.css({
+            width: dimensions.width,
+            height: dimensions.height,
+            left: ($(window).width() - dimensions.width) / 2,
+            top: ($(window).height() - dimensions.height) / 2
+        });
+
+        $("body").append(canvas);
+    };
+
+    const renderPredictions = function (predictions) {
+        var dimensions = videoDimensions(video);
+
+        var scale = 1;
+
+        ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+
+        predictions.forEach(function (prediction) {
+            const x = prediction.bbox.x;
+            const y = prediction.bbox.y;
+
+            const width = prediction.bbox.width;
+            const height = prediction.bbox.height;
+
+            // Draw the bounding box.
+            ctx.strokeStyle = prediction.color;
+            ctx.lineWidth = 4;
+            ctx.strokeRect(
+                (x - width / 2) / scale,
+                (y - height / 2) / scale,
+                width / scale,
+                height / scale
+            );
+
+            // Draw the label background.
+            ctx.fillStyle = prediction.color;
+            const textWidth = ctx.measureText(prediction.class).width;
+            const textHeight = parseInt(font, 10); // base 10
+            ctx.fillRect(
+                (x - width / 2) / scale,
+                (y - height / 2) / scale,
+                textWidth + 8,
+                textHeight + 4
+            );
+        });
+
+        predictions.forEach(function (prediction) {
+            const x = prediction.bbox.x;
+            const y = prediction.bbox.y;
+
+            const width = prediction.bbox.width;
+            const height = prediction.bbox.height;
+
+            // Draw the text last to ensure it's on top.
+            ctx.font = font;
+            ctx.textBaseline = "top";
+            ctx.fillStyle = "#000000";
+            ctx.fillText(
+                prediction.class,
+                (x - width / 2) / scale + 4,
+                (y - height / 2) / scale + 1
+            );
+        });
+    };
+
+    var prevTime;
+    var pastFrameTimes = [];
+    const detectFrame = function () {
+        if (!model) return requestAnimationFrame(detectFrame);
+
+        model
+            .detect(video)
+            .then(function (predictions) {
+                requestAnimationFrame(detectFrame);
+                renderPredictions(predictions);
+
+                if (prevTime) {
+                    pastFrameTimes.push(Date.now() - prevTime);
+                    if (pastFrameTimes.length > 30) pastFrameTimes.shift();
+
+                    var total = 0;
+                    _.each(pastFrameTimes, function (t) {
+                        total += t / 1000;
+                    });
+
+                    var fps = pastFrameTimes.length / total;
+                    $("#fps").text(Math.round(fps));
+                }
+                prevTime = Date.now();
+            })
+            .catch(function (e) {
+                console.log("CAUGHT", e);
+                requestAnimationFrame(detectFrame);
+            });
+    };
+});
